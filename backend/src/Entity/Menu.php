d3f7ee4452e316c750135fc36766455511c4a137
andy agustian <andi.agustian@gmail.com>
2019-07-26 09:35:12 +0700

    create sample api

---
+++ backend/src/Entity/Menu.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass="App\Repository\MenuRepository")
+ */
+class Menu
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=40)
+     */
+    private $menu;
+
+    /**
+     * @ORM\Column(type="string", length=30, nullable=true)
+     */
+    private $icon;
+
+    /**
+     * @ORM\Column(type="string", length=40, nullable=true)
+     */
+    private $url;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getMenu(): ?string
+    {
+        return $this->menu;
+    }
+
+    public function setMenu(string $menu): self
+    {
+        $this->menu = $menu;
+
+        return $this;
+    }
+
+    public function getIcon(): ?string
+    {
+        return $this->icon;
+    }
+
+    public function setIcon(?string $icon): self
+    {
+        $this->icon = $icon;
+
+        return $this;
+    }
+
+    public function getUrl(): ?string
+    {
+        return $this->url;
+    }
+
+    public function setUrl(?string $url): self
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+}
